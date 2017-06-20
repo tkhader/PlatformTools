62b652ddc5d771aa566cc96ade5fea009432469c
Prabakaran Santhanam <PSanthanam@altimetrik.com>
2017-06-20 14:54:59 +0530

    initial commit

---
+++ app/js/components/Global/Menu.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+export default class Menu extends Component {
+  render() {
+    return (
+      <div className='Menu'>
+      	<h2>
+        	Main Menu
+        </h2>
+      </div>
+    );
+  }
+}

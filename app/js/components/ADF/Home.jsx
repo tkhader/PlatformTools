1b589cfa8a0ff14023dff477890464ece2c388b5
Prabakaran Santhanam <PSanthanam@altimetrik.com>
2017-06-21 12:36:35 +0530

    change the color of the tiles and added navbar on all pages

--- app/js/components/ADF/Home.jsx
+++ app/js/components/ADF/Home.jsx
@@ -10,7 +10,19 @@ export default class ADFHome extends Component {
     return (
         <div className="container">
         <div>
-          <a href="/">Home</a>
+          <nav className="navbar navbar-default">
+            <div className="container-fluid">
+              <div className="navbar-header">
+                <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                  <span className="sr-only">Toggle navigation</span>
+                  <span className="icon-bar"></span>
+                  <span className="icon-bar"></span>
+                  <span className="icon-bar"></span>
+                </button>
+                <a className="navbar-brand" href="/">Apps</a>
+              </div>
+            </div>
+          </nav>
         </div>
         <Panel header="ADF">
         <div className="container">
--- app/js/components/API/Home.jsx
+++ app/js/components/API/Home.jsx
@@ -9,7 +9,21 @@ export default class APIHome extends Component {
   render() {
     return (
         <div className="container">
-        <a href="/">Home</a>
+        <div>
+          <nav className="navbar navbar-default">
+            <div className="container-fluid">
+              <div className="navbar-header">
+                <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                  <span className="sr-only">Toggle navigation</span>
+                  <span className="icon-bar"></span>
+                  <span className="icon-bar"></span>
+                  <span className="icon-bar"></span>
+                </button>
+                <a className="navbar-brand" href="/">Apps</a>
+              </div>
+            </div>
+          </nav>
+        </div>
         <Panel header="API E&M Track">
         <div className="container">
           <ul className="nav nav-pills nav-stacked col-md-2">
